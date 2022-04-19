671589ed06b4fd2af585947cb284956b6a21d618
ruslan4432013 <ruslan4432013@gmail.com>
2022-04-19 10:24:31 +0300

    oop beginning

--- eShop/script.js
+++ eShop/script.js
@@ -1,28 +1,65 @@
-const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 350},
-    {title: 'Shoes', price: 250},
-];
+class GoodsItem {
+    constructor(title, price, img = 'http://placekitten.com/g/160/120') {
+        this.title = title;
+        this.price = price;
+        this.img = img
+    }
 
-
-const renderGoodsItem = (item, img = 'http://placekitten.com/g/160/120') => {
-    return `<div class="col-sm-3 my-3">
+    render() {
+        return (
+            `<div class="col-sm-3 my-3">
             <div class="card"">
-            <img src="${img}" class="card-img-top" alt="kitten">
+            <img src="${this.img}" class="card-img-top" alt="kitten">
               <div class="card-body">
-                <h1 class="card-title">${item.title}</h1>
+                <h1 class="card-title">${this.title}</h1>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <h5>Price: ${item.price} ₽</h5>
+                <h5>Price: ${this.price} ₽</h5>
                 <a href="#" class="btn btn-primary">Go to item</a>
               </div>
             </div>
-        </div>`;
-};
-
-const renderGoodsList = (items) => {
-    document.querySelector('.goods-list').innerHTML = items.map(item => renderGoodsItem(item)).join('');
+        </div>`)
+    }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+    constructor() {
+        this.goods = []
+    }
 
+    fetchGoods() {
+        this.goods = [
+            {title: 'Shirt', price: 150},
+            {title: 'Socks', price: 50},
+            {title: 'Jacket', price: 350},
+            {title: 'Shoes', price: 250},
+        ]
+    }
+
+    totalPrice() {
+        return this.goods.reduce((total, item) => total + item.price, 0);
+    }
+
+    render() {
+        let listHTML = ''
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHTML += goodItem.render()
+        });
+        document.querySelector('.goods-list').innerHTML = listHTML
+    }
+}
+
+class CartItems {
+    constructor() {
+    }
+}
+
+class CartItem {
+    constructor() {
+    }
+}
+
+const list = new GoodsList();
+list.fetchGoods()
+console.log(list.totalPrice())
+list.render()
