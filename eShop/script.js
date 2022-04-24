7f3bd6e24904d5660665fa68d1383b8e691b1e51
ruslan4432013 <ruslan4432013@gmail.com>
2022-04-24 21:43:16 +0300

    add Cart and use Ajax requests

--- eShop/script.js
+++ eShop/script.js
@@ -1,6 +1,8 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
 class GoodsItem {
-    constructor(title, price, img = 'http://placekitten.com/g/160/120') {
-        this.title = title;
+    constructor(product_name, price, img = 'http://placekitten.com/g/160/120') {
+        this.product_name = product_name;
         this.price = price;
         this.img = img
     }
@@ -11,7 +13,7 @@ class GoodsItem {
             <div class="card"">
             <img src="${this.img}" class="card-img-top" alt="kitten">
               <div class="card-body">
-                <h1 class="card-title">${this.title}</h1>
+                <h1 class="card-title">${this.product_name}</h1>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <h5>Price: ${this.price} ₽</h5>
                 <a href="#" class="btn btn-primary">Go to item</a>
@@ -27,13 +29,11 @@ class GoodsList {
     }
 
     fetchGoods() {
-        this.goods = [
-            {title: 'Shirt', price: 150},
-            {title: 'Socks', price: 50},
-            {title: 'Jacket', price: 350},
-            {title: 'Shoes', price: 250},
-        ]
+        return makeGETRequest(`${API_URL}/catalogData.json`).then((data) => {
+            this.goods = data
+        })
     }
+
 
     totalPrice() {
         return this.goods.reduce((total, item) => total + item.price, 0);
@@ -42,24 +42,73 @@ class GoodsList {
     render() {
         let listHTML = ''
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
+            const goodItem = new GoodsItem(good.product_name, good.price);
             listHTML += goodItem.render()
         });
         document.querySelector('.goods-list').innerHTML = listHTML
+
     }
 }
 
 class CartItems {
     constructor() {
+        this.amount = 0
+        this.countGoods = 0
+        this.cartItems = []
     }
+
+
+    fetchCart() {
+        makeGETRequest(`${API_URL}/getBasket.json`).then((data) => {
+            this.amount = data.amount
+            this.countGoods = data.countGoods
+            this.cartItems = data.content.map(item => {
+                new CartItem(item.id, item.product_name, item.price, item.quantity)
+            })
+        })
+    }
+
+    addCartItem(item) {
+        makeGETRequest(`${API_URL}/addToBasket.json`).then((data) => {
+            if (data.result === 1) {
+                this.amount += item.price
+                this.countGoods += item.quantity
+                this.cartItems.push(item)
+            }
+        }).catch(error => console.log(error))
+    }
+
+    removeCartItem(item) {
+        makeGETRequest(`${API_URL}/deleteFromBasket.json`).then((data) => {
+            if (data.result === 1) {
+                const index = this.cartItems.map(item => item.id).indexOf(item.id);
+                if (index > -1) {
+                    this.cartItems.splice(index, 1)
+                    this.amount -= item.price
+                    this.countGoods -= item.quantity
+                }
+            }
+        })
+    }
+
 }
 
 class CartItem {
-    constructor() {
+    constructor(id, product_name, price, quantity) {
+        this.id = id
+        this.product_name = product_name
+        this.price = price
+        this.quantity = quantity
     }
 }
 
+function makeGETRequest(url) {
+    return fetch(url).then((response) => response.json())
+}
+
+
 const list = new GoodsList();
-list.fetchGoods()
-console.log(list.totalPrice())
-list.render()
+list.fetchGoods().then(() => list.render())
+
+const mainCart = new CartItems()
+mainCart.fetchCart()
